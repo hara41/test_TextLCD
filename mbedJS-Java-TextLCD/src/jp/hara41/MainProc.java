6545def62471eb7fb222c9cd6f8028a1b952757e
hara41 <hara.shinichi@gmail.com>
2014-08-03 08:35:29 +0900

    write documents (5)

---
+++ mbedJS-Java-TextLCD/src/jp/hara41/MainProc.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package jp.hara41;
+
+import jp.nyatla.mimic.mbedjs.javaapi.*;
+
+/**
+ * @author hara4_000
+ *
+ */
+public class MainProc {
+
+
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			Mcu mcu=new Mcu("10.0.0.2");
+			
+			TextLCD lcd = new TextLCD(mcu , PinName.p24, PinName.p26,
+					PinName.p27, PinName.p28, PinName.p29, PinName.p30,TextLCD.LCDType.LCD16x2);
+			
+			lcd.putc('T');
+			lcd.putc('E');
+			lcd.putc('S');
+			lcd.putc('T');
+		
+			mcu.close();
+			System.out.println("done");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
